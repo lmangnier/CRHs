cb586e13b05aace57d04cc574930035535e03d84
abureau <abureau6@gmail.com>
2021-02-11 14:14:31 -0500

    Changing path to input data

--- SNPs_enrichment.R
+++ SNPs_enrichment.R
@@ -23,21 +23,22 @@ library(mice)
 
 set.seed(1258)
 setwd("/home/loic/Documents/HiC/data/4Script/NEU")
+source("HiCrn.R")
 
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 genes.hg19 <- genes(txdb)
 symbol <- select(org.Hs.eg.db,keys = genes.hg19$gene_id, columns = c("SYMBOL"), keytype = "ENTREZID")
 genes.hg19$geneSymbol <- symbol$SYMBOL
 
-regul.promoters.ABC = process.ABC("ABC/EnhancerPredictions.txt")
+regul.promoters.ABC = process.ABC("input_data/EnhancerPredictions.txt")
 # all.putative.ABC = read.table("ABC/EnhancerPredictionsAllPutative.txt.gz", header = T)
 # all.putative.ABC = all.putative.ABC[all.putative.ABC$chr!="chrX",]
 # 
 # all.putative.ABC.candidates = all.putative.ABC[!all.putative.ABC$name%in%regul.promoters.ABC$name&all.putative.ABC$class!="promoter",]
 
-hic.loops = read.table("allloops_HiCCUPS/enriched_pixels_10000.bedpe", header=T)
+hic.loops = read.table("input_data/enriched_pixels_10000.bedpe", header=T)
 
-peaks.NEU = import("Epigenetic/NEU_DNAse.macs2_peaks.narrowPeak.sorted.candidateRegions.bed", format="bed")
+peaks.NEU = import("input_data/NEU_DNAse.macs2_peaks.narrowPeak.sorted.candidateRegions.bed", format="bed")
 peaks.NEU = peaks.NEU[seqnames(peaks.NEU)%in%paste0("chr", 1:22)]
 
 GRanges.loops.bin1 = GRanges(seqnames = hic.loops$chr1, ranges=IRanges(start=hic.loops$x1, end=hic.loops$x2))
@@ -69,7 +70,7 @@ promoters.candidates.DNAse = make.comparable.set(unique.Promoters.DNAse,method="
 
 
 #Fichier de variants clumpe
-SCZ3.all.clumped = read.table("../Genetic/PGC3_SCZ_wave3_public.clumped.v2.tsv", header=T, fill = T)
+SCZ3.all.clumped = read.table("input_data/Genetic/PGC3_SCZ_wave3_public.clumped.v2.tsv", header=T, fill = T)
 GRanges.snps.SCZ3.clumped = GRanges(seqnames=paste0("chr",SCZ3.all.clumped$CHR), ranges=IRanges(start=SCZ3.all.clumped$BP, end=SCZ3.all.clumped$BP, names=SCZ3.all.clumped$SNP), pval=SCZ3.all.clumped$P)
 
 #Differents seuils de significativite consideres
